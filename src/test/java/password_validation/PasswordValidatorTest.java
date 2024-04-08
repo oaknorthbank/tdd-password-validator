f558d2ac46af8957d17a36182defabf4068aaaf6
ankit.kumar1 <ankit.kumar1@oaknorth.co.uk>
2024-04-08 20:14:20 +0530

    Added TDD practice set for Password validator

---
+++ src/test/java/password_validation/PasswordValidatorTest.java
@@ -0,0 +1,13 @@
+package password_validation;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.jupiter.api.Test;
+
+public class PasswordValidatorTest {
+
+  @Test
+  public void fix_me_and_rename() {
+    assertFalse(true);
+  }
+}
